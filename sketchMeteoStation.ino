9dc2f8fd6f44ce65ca90c21958df2a4a6ce515b1
Dmitriy Dixmod Popov <dixmod@gmail.com>
2019-01-12 01:09:01 +0300

    Split information about temperature, pressure and humidity. Add frames

--- sketchMeteoStation.ino
+++ sketchMeteoStation.ino
@@ -17,7 +17,7 @@ BME280I2C bme;
 // Temperature Oversampling Rate, Humidity Oversampling Rate, Pressure Oversampling Rate, Mode, Standby Time, Filter, SPI Enable, BME280 Address
 // BME280I2C bme(0x1, 0x1, 0x1, 0x1, 0x5, 0x0, false, 0x77); // Version for SparkFun BME280
   
-extern uint8_t BigNumbers[];
+extern uint8_t MediumNumbers[];
 extern uint8_t SmallFont[];
 
 struct {
@@ -54,68 +54,16 @@ void setup() {
 }
 
 void loop() {
+  
   bool metric = true;
-  float temp(NAN), hum(NAN), pres(NAN);
+  float val(NAN), temp(NAN), hum(NAN), pres(NAN), minVal(NAN), maxVal(NAN);
+  String title(""), unit("");
+  int leftTitle(1);
   uint8_t pressureUnit(0); // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
 
   bme.read(pres, temp, hum, metric, pressureUnit);
 //  temp -= 0.3; // correct temp
-  pres /= 133.3; // convert to mmHg
-
-  myOLED.setBrightness(10);
-  myOLED.clrScr();
-  
-  myOLED.setFont(BigNumbers);  
-  myOLED.print(String(temp, 1), 0, 0);
-  myOLED.print(String(hum,  0), 92, 0);
-  myOLED.print(String(pres, 1), 42, 40);
-
-  myOLED.setFont(SmallFont);
-  myOLED.print("~C", 56, 0);
-  myOLED.print("%", 122, 0);
-  myOLED.print("MM", 114, 58);
-  myOLED.update();
-
-  avrg.temp += temp;
-  avrg.hum += hum;
-  avrg.pres += pres;
-  avrg.counter++;
-
-  if (fastMode && avrg.counter >= STORAGE_TIME) {
-    fastMode = false;
-    for (int i = 0; i < PLOT_LEN - 1; i++) {
-      infoArr[i].temp = 0;
-      infoArr[i].hum = 0;
-      infoArr[i].pres = 0;
-    }
-  }
-
-  if (fastMode || avrg.counter >= STORAGE_TIME) {
-    if (avrg.counter >= STORAGE_TIME) {
-      temp = avrg.temp / avrg.counter;
-      hum = avrg.hum / avrg.counter;
-      pres = avrg.pres / avrg.counter;
-      avrg.temp = 0;
-      avrg.hum = 0;
-      avrg.pres = 0;
-      avrg.counter = 0;
-    }
-    for (int i = 1; i < PLOT_LEN; i++) {
-      infoArr[i - 1] = infoArr[i];
-    }
-    infoArr[PLOT_LEN - 1].temp = round(temp) + 50;
-    infoArr[PLOT_LEN - 1].pres = round(pres) - 650;
-    infoArr[PLOT_LEN - 1].hum = round(hum);
-  }
-  delay(5000);
-
-  /*
-    Graph
-  */
-
-  if (wait_cnt > 3) {
-    wait_cnt = 0;
-    myOLED.clrScr();
+  pres /= 133.3; // convert to mmHg  
 
     byte minTemp = 255;
     byte minHum = 255;
@@ -123,22 +71,118 @@ void loop() {
     byte maxTemp = 0;
     byte maxHum = 0;
     byte maxPres = 0;
-
+     
+    avrg.temp += temp;
+    avrg.hum += hum;
+    avrg.pres += pres;
+    avrg.counter++;
+  
+    if (fastMode && avrg.counter >= STORAGE_TIME) {
+      fastMode = false;
+      for (int i = 0; i < PLOT_LEN - 1; i++) {
+        infoArr[i].temp = 0;
+        infoArr[i].hum = 0;
+        infoArr[i].pres = 0;
+      }
+    }
+  
+    if (fastMode || avrg.counter >= STORAGE_TIME) {
+      if (avrg.counter >= STORAGE_TIME) {
+        temp = avrg.temp / avrg.counter;
+        hum = avrg.hum / avrg.counter;
+        pres = avrg.pres / avrg.counter;
+        avrg.temp = 0;
+        avrg.hum = 0;
+        avrg.pres = 0;
+        avrg.counter = 0;
+      }
+      for (int i = 1; i < PLOT_LEN; i++) {
+        infoArr[i - 1] = infoArr[i];
+      }
+      infoArr[PLOT_LEN - 1].temp = round(temp) + 50;
+      infoArr[PLOT_LEN - 1].pres = round(pres) - 650;
+      infoArr[PLOT_LEN - 1].hum = round(hum);
+    }
+  
     for (int i = PLOT_LEN - 1; i >= 0 ; i--) {
       if (infoArr[i].temp == 0 && infoArr[i].hum == 0 && infoArr[i].pres == 0) break;
-
+  
       if (infoArr[i].temp < minTemp) minTemp = infoArr[i].temp;
       if (infoArr[i].hum < minHum) minHum = infoArr[i].hum;
       if (infoArr[i].pres < minPres) minPres = infoArr[i].pres;
-
+  
       if (infoArr[i].temp > maxTemp) maxTemp = infoArr[i].temp;
       if (infoArr[i].hum > maxHum) maxHum = infoArr[i].hum;
       if (infoArr[i].pres > maxPres) maxPres = infoArr[i].pres;
     }
+    
     if (maxTemp - minTemp < 10) maxTemp = minTemp + 10;
     if (maxHum - minHum < 10) maxHum = minHum + 10;
     if (maxPres - minPres < 10) maxPres = minPres + 10;
 
+
+  myOLED.setBrightness(10);
+  myOLED.clrScr();
+
+  if(wait_cnt == 0){
+      title = "TEMPERATURE";
+      leftTitle = 32;
+      
+      unit = "~C";
+      val = temp; 
+
+      minVal = minTemp-50;
+      maxVal = maxTemp-50;
+  }else if(wait_cnt == 1){
+      title = "HUMIDITY";
+      leftTitle = 40;
+      
+      unit = "%";
+      val = hum;
+
+      minVal = minHum;
+      maxVal = maxHum;
+  }else if(wait_cnt == 2){
+      title = "PRESSURE";
+      leftTitle = 40;
+      
+      unit = "MM";
+      val = pres;
+
+      minVal = minPres+650;
+      maxVal = maxPres+650;
+  }
+  
+  myOLED.setFont(SmallFont);
+  myOLED.drawRoundRect(1, 1, 127, 15); // черный прямоугольник со скругленными вершинами.       
+  myOLED.print(title, leftTitle, 5);  
+
+  myOLED.drawRoundRect(1, 16, 80, 63); // черный прямоугольник со скругленными вершинами.
+  myOLED.setFont(MediumNumbers);  
+  myOLED.print(String(val, 1), 10, 31);
+
+  myOLED.setFont(SmallFont);
+  myOLED.print(unit, 64, 20);
+
+  myOLED.drawRoundRect(82, 16, 127, 63); // черный прямоугольник со скругленными вершинами.
+  myOLED.setFont(SmallFont);
+  myOLED.print("MIN", 95, 20);
+  myOLED.print(String(minVal, 1), 85, 32);
+
+  myOLED.print("MAX", 95, 43);
+  myOLED.print(String(maxVal, 1), 85, 53);  
+
+  myOLED.update();
+  
+  delay(15000);
+
+  /*
+    Graph
+  */
+  wait_cnt++;
+  if (wait_cnt > 2) {
+    wait_cnt = 0;
+    myOLED.clrScr();
 
     myOLED.setFont(SmallFont);
     myOLED.print(String(minTemp - 50), 0, 12);
@@ -163,9 +207,9 @@ void loop() {
 
     myOLED.update();
 
-    delay(5000);
+    delay(15000);
   }
 
-  wait_cnt++;
+  
 
 }
